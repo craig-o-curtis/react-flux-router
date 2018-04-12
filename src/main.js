a607afdac465f47fb369e699842ecf75143bc64d
Craig Curtis <craigcurtis@Craigs-MacBook-Pro.local>
2018-04-11 21:06:30 -0700

    Add vanilla routing in React with window hashchange event

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
+"use strict";
+// not strict mode
 $ = jQuery = require('jquery');
 
 var React = require('react');
-var Home = require('./components/homePage'); 
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-React.render(<Home />, document.getElementById('app'));
+var App = React.createClass({
+	render: function () {
+		var Child = null;
+
+		switch (this.props.route) {
+			case 'about': Child = About; break;
+			default: Child = Home;
+		}
+
+		return (
+			<div>
+				<Child />
+			</div>
+		);
+	}
+});
+
+function render() {
+	var route = window.location.hash.substr(1);
+	
+	React.render(<App route={route} />, document.getElementById('app'));
+}
+
+// event listener for URL change
+window .addEventListener('hashchange', render);
+render();
+
+// React.render(<Home />, document.getElementById('app'));
